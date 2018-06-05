891d4771817ef38579e653d23d87eb1d62e085b9
Jeff Sirocki <jsirocki@gmail.com>
2018-06-05 14:13:19 +0200

    list to train places

--- vae/train.py
+++ vae/train.py
@@ -45,6 +45,12 @@ def main():
     FLAGS.list_dir = 'data/imglist/lfw/'
     FLAGS.pc_dir = 'data/pcomp/lfw/'
   #add other datasets here
+  elif(sys.argv[1] == 'places'):
+    FLAGS.updates_per_epoch = 38
+    FLAGS.log_interval = 12
+    FLAGS.out_dir = 'data/output/places/'
+    FLAGS.list_dir = 'data/imglist/places/'
+    FLAGS.pc_dir = 'data/pcomp/places/'
   else:
     raise NameError('[ERROR] Incorrect dataset key')
 
